8333d16241ff9cd0e17e48352daef58f0dc92593
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-02 15:57:54 +0300

    Initial commit

---
+++ bookstore/books/urls.py
@@ -0,0 +1,12 @@
+from importlib.resources import path
+from xml.etree.ElementInclude import include
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    
+    path('',views.index, name='index')
+
+]
