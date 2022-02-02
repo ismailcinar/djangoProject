6bf6e9d8f18e8b9a81873a8cb96b17bfb277d8be
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-02 16:24:10 +0300

    Update models.py

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Author(models.Model):
+    def __str__(self):
+        return self.name
+    
     name = models.CharField(max_length=50)
     created = models.DateTimeField('date created')
     
 class Book(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=50)
     created = models.DateTimeField('data created')
     author = models.ForeignKey(Author,on_delete= models.CASCADE)
