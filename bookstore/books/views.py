f256d529f20bffd7e51f5632effdc396d88e1cbe
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-04 00:08:24 +0300

    something -

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,13 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Author
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Main")
 def authors(request):
-    return HttpResponse("Authors")
+    template = loader.get_template('authors.html')
+    context = {
+        'authors_list' : Author.objects.all()
+    }
+    return render(request,'authors.html',context)
+    #return HttpResponse(template.render(context,request))
 def books(request):
     return HttpResponse("Books")
 def authorDetails(request,authorId):
-    return HttpResponse("Author details :"+str(authorId))
+   #  template = loader.get_template('authorDetail.html')
+     context = {
+        'author_detail' : Author.objects.get(pk=authorId)
+    } 
+    return render(request,'authorDetail.html',context)
