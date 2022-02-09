5e4ad4b93b90d246cc812805c02c1879eb1c9a09
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-09 18:33:57 +0300

    views

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+posts = [
+    {
+        'author' : 'İsmailÇ',
+        'title' : 'Blog Post',
+        'content' : 'First post content',
+        'date_posted' : 'November 11, 1998',
+    },
+      {
+        'author' : 'KazımK',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'December 11, 1998',
+    }
+]
 
+
+def home(request):
+   # return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+  #  return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
