bdff43e849e06bb8abe63786dbc054948df49694
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-10 21:24:33 +0300

    +

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 posts = [
     {
         'author' : 'İsmailÇ',
@@ -20,7 +20,7 @@ posts = [
 def home(request):
    # return HttpResponse('<h1>Blog Home</h1>')
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html',context)
 def about(request):
