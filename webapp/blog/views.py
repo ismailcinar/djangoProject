e818b3d01954f358b0bbeda547a170d54a637dce
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-11 19:07:23 +0300

    User Registration

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-posts = [
-    {
-        'author' : 'İsmailÇ',
-        'title' : 'Blog Post',
-        'content' : 'First post content',
-        'date_posted' : 'November 11, 1998',
-    },
-      {
-        'author' : 'KazımK',
-        'title' : 'Blog Post 2',
-        'content' : 'Second post content',
-        'date_posted' : 'December 11, 1998',
-    }
-]
+# posts = [
+#     {
+#         'author' : 'İsmailÇ',
+#         'title' : 'Blog Post',
+#         'content' : 'First post content',
+#         'date_posted' : 'November 11, 1998',
+#     },
+#       {
+#         'author' : 'KazımK',
+#         'title' : 'Blog Post 2',
+#         'content' : 'Second post content',
+#         'date_posted' : 'December 11, 1998',
+#     }
+# posts:posts]
 
 
 def home(request):
