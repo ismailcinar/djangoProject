e694fba1c1854475f7dc45cbab4fb31de9f5f10d
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-13 21:00:29 +0300

    User Profile

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
