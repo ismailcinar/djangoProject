467ccba7d4f6cfc8ec35f49edf8c2b73a16d17b8
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-19 23:23:07 +0300

    .

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -37,11 +37,16 @@ urlpatterns = [
              template_name='users/password_reset_done.html'
         ),
          name='password_reset_done'),
-    path('password-reset-confirm/uidb64/token/', 
+    path('password-reset-confirm/<uidb64>/<token>/', 
          auth_views.PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html'
         ),
          name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('', include('blog.urls')),
 
 ]
