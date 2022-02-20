44e1646a2f7912d654e40a9d1dcf6eeba31c0d41
İsmail Çınar <ismailcinar99@gmail.com>
2022-02-20 23:46:05 +0300

    .

--- djcrm/djcrm/settings.py
+++ djcrm/djcrm/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'leads' # // when you add new ..
+    'leads', # // when you add new ..
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
